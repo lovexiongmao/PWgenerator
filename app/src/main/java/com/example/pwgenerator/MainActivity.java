3485306f6cb5f06544f7d5b6b82e0de212a0820a
chengyinkai <chengyinkai@bytedance.com>
2022-08-11 00:32:17 +0800

    调通了逻辑

--- app/src/main/java/com/example/pwgenerator/MainActivity.java
+++ app/src/main/java/com/example/pwgenerator/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.pwgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void TransformCode(View v) {
+        et = findViewById(R.id.input);
+        String str = et.getText().toString().trim();
+        tv = findViewById(R.id.hashvalue);
+        Toast.makeText(this, "欢迎你，新世界管理者", Toast.LENGTH_SHORT).show();
+        tv.setText(str);
+    }
+
 }
